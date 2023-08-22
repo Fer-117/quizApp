6a707ea75ff16462eb9a6c1ee9d9b5d998356201
Fer-117 <fsg11497@gmail.com>
2023-08-22 15:27:46 -0600

    fixed high score page

--- JS/script.js
+++ JS/script.js
@@ -1,44 +1,40 @@
-const questionTitle = document.getElementById("title");
-const optionContainer = document.getElementById("optionContainer");
-const buttons = document.querySelectorAll(".question");
-const answerMessage = document.getElementById("answerMessage");
+const questionTitle = document.getElementById('title');
+const optionContainer = document.getElementById('optionContainer');
+const buttons = document.querySelectorAll('.question');
+const answerMessage = document.getElementById('answerMessage');
 
-// TODO
-// target the div, input and button for the name
-// collect the value of the input on click of "save name" button
-// on the same click event, remove the hide class from the questions container and add it to the nameInput container
-// the colected name from the input should go in an object which will later have the timer value as well
-// at the end of thequiz add the timer value with the time left, to the forementioned object
-// push the object inside the highscores array from Local Storage
-// redirect to the Highscores page and retrieve that array (highscores) from ls
+const landingPage = document.querySelector('.landingPageSection');
+const questionSection = document.querySelector('.questionSection');
+const finalScore = document.querySelector('.finalScore');
+const highScoreSection = document.querySelector('.highScoreSection');
+const startQuizBtn = document.getElementById('startQuiz');
+
+let scoreText = document.querySelector('.score');
+
+let showTimer = document.querySelector('.timer');
+let countDown = document.querySelector('.timerCountdown');
+let secondsLeft = 50;
+var timerInterval;
+
+const inputInitials = document.getElementById('inputInitials');
+const submitBtn = document.getElementById('submitBtn');
+
+const goBack = document.getElementById('goBack');
 
 let questionArrayCounter = 0;
 
 let questionCounter = 0;
 
 let answersArray = [];
-localStorage.setItem("answers", answersArray);
 
-if (buttons.length > 0) {
-  console.log("here it is", buttons);
-}
-const highscoresarray = [
-  {
-    name: "",
-    score: 0,
-  },
-  {
-    name: "Fernndo",
-    score: 0,
-  },
-  ,
-];
+var highscoresarray =
+  JSON.parse(window.localStorage.getItem('highscoresarray')) || [];
 
 if (optionContainer) {
-  optionContainer.addEventListener("click", (event) => {
+  optionContainer.addEventListener('click', (event) => {
     const element = event.target;
     // console.log(element);
-    if (element.matches("button")) {
+    if (element.matches('button')) {
       const option = element.innerHTML;
       // console.log("inside the if of the optionContainer event listener");
       // console.log(`the current question counter is: ${questionCounter}`);
@@ -49,59 +45,49 @@ if (optionContainer) {
 
       if (option === questionArray[questionCounter].answer) {
         // show the message for congratulations and continue to the next set of questions
-        showAnswerMessage("Congratulations! Your answered correctly.");
-        answersArray.push(option);
-        localStorage.setItem("answers", answersArray);
+        showAnswerMessage('Congratulations! Your answered correctly.');
       } else {
         //show the message for the wrong answer
         // reduce 10 seconds from the timer
         showAnswerMessage();
-        answersArray.push(option);
-        localStorage.setItem("answers", answersArray);
       }
     } else {
-      answerMessage.innerHTML = "you are finished";
-      questionCounter = 4;
       endQuiz();
-      return;
     }
   });
 }
+// cycles to the next question from the questionArray and
+// calls the setNextQuestion function to set the message.
 function nextQuestion() {
   // event.stopPropagation();
   if (questionCounter < 4) {
     questionCounter++;
-    answerMessage.setAttribute("style", "visibility: hidden");
-    console.log(
-      `Inside the nextQuestion function, the counter is ${questionCounter}`
-    );
+    answerMessage.setAttribute('style', 'display: none');
     setNextQuestion();
   } else {
-    answerMessage.innerHTML = "you are finished";
-    return;
+    answerMessage.setAttribute('style', 'display: none');
+    setNextQuestion();
+    endQuiz();
   }
 }
 
 function showAnswerMessage(message) {
   if (message) {
-    console.log("Inside showAnswerMessage function");
-    answerMessage.innerHTML = message;
-    answerMessage.setAttribute("style", "visibility: visible");
     // set a timer to change to the next question.
-
+    answerMessage.innerHTML = message;
+    answerMessage.setAttribute('style', 'display: block');
     setTimeout(nextQuestion, 2000);
   } else {
     answerMessage.innerHTML =
-      "Sorry that answer is incorrect, better luck next time!";
-    answerMessage.setAttribute("style", "visibility: visible");
+      'Sorry that answer is incorrect, better luck next time!';
+    answerMessage.setAttribute('style', 'display: block');
+
     setTimeout(nextQuestion, 2000);
   }
 }
 
+//Todo: loop throught the object array and display the text on the screen
 function setNextQuestion() {
-  console.log("Entered loop question Array function");
-  //Todo: loop throught the object array and display the text on the screen
-
   if (questionTitle) {
     questionTitle.innerHTML = questionArray[questionCounter].question;
   }
@@ -112,9 +98,58 @@ function setNextQuestion() {
   }
 }
 
-const endQuiz = () => {
-  setTimeout(() => {}, 2000);
-  // we're setting the ddress for our website -- redirecting to the highscore page
-  window.location.href = "highScore.html";
-};
-setNextQuestion();
+function startQuiz() {
+  landingPage.setAttribute('style', 'display:none');
+  questionSection.setAttribute('style', 'display:block');
+  setNextQuestion();
+  setTime();
+}
+
+function setTime() {
+  if (questionCounter >= 4) {
+    return;
+  }
+  showTimer.classList.remove('hide');
+
+  // Sets interval in variable
+  timerInterval = setInterval(function () {
+    secondsLeft--;
+    countDown.textContent = secondsLeft;
+
+    if (secondsLeft === 0 || questionCounter > 4) {
+      // Stops execution of action at set interval
+      // Calls function to create and append image
+      endQuiz();
+    }
+  }, 1000);
+}
+
+function endQuiz() {
+  clearInterval(timerInterval);
+  questionSection.setAttribute('style', 'display:none');
+  finalScore.classList.remove('hide');
+  let thisScore = (scoreText.innerHTML = secondsLeft);
+}
+
+function updateHighscore() {
+  let initials = inputInitials.value;
+  var newHighscore = {
+    name: initials,
+    score: scoreText.textContent,
+  };
+
+  highscoresarray.push(newHighscore);
+  // console.log(highscoresarray);
+  localStorage.setItem('highscoresarray', JSON.stringify(highscoresarray));
+}
+
+startQuizBtn.addEventListener('click', startQuiz);
+submitBtn.addEventListener('click', updateHighscore);
+
+goBack.addEventListener('click', () => {
+  window.location.reload();
+  // highScoreSection.setAttribute("style", "display:none");
+  // questionSection.setAttribute("style", "display:none");
+  // finalScore.setAttribute("style", "display:none");
+  // landingPage.setAttribute("style", "display:block");
+});
